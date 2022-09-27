67361d6ce563ffe2f4314abf896dc80676188a50
Yassine Ait Jeddi <48790841+Altimis@users.noreply.github.com>
2022-09-27 17:23:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -889,7 +889,7 @@ def test():
 if __name__ == "__main__":
     #test()
     #main()
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=80)
     #scraper = Scraper(barcodelookup_url=config.barcodelookup_url, gunengine_url=config.gunengine_url,
     #                  gundeals_url=config.gundeals_url, wikiarms_url=config.wikiarms_url)
     #driver = scraper.init_driver(proxy_server='185.33.85.114:49155')
