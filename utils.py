5c9cb409deb69440f3eb81699c9e138f61d8e0e2
yassine ait jeddi <yassine.aitjeddi@mosaiclab.io>
2022-10-17 15:37:58 +0100

    test

--- utils.py
+++ utils.py
@@ -99,7 +99,7 @@ def init_driver(is_proxy=False, proxy=None, proxy_server=None):
     while not done and attempt < 4:
         options = uc.ChromeOptions()
         #options = Options()
-        #options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         if config.use_proxy:
@@ -109,8 +109,8 @@ def init_driver(is_proxy=False, proxy=None, proxy_server=None):
                 print("using proxy_server : ", proxy_server)
                 options.add_argument(f"--proxy-server={proxy_server}")
         try:  # will patch to newest Chrome driver version
-            driver = uc.Chrome(use_subprocess=True, options=options,
-                               driver_executable_path=config.driver_executable_path)#, driver_executable_path)
+            driver = uc.Chrome(options=options)
+                              # driver_executable_path=config.driver_executable_path)#, driver_executable_path)
             done = True
         except:  # newest driver version not matching Chrome version
             err = traceback.format_exc()
